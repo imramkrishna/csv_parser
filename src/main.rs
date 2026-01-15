9601950854c01851f693509819e6a8c7433ec167
Ram Krishna Yadav <itsramky234@gmail.com>
2026-01-16 00:42:26 +0545

    fix :  added data saving and proper formatting for json.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod parse_csv;
 mod get_headers;
 mod get_csv_data;
+mod convert_to_json;
+mod save_file_to_json;
 
 fn main() {
-    parse_csv::parse_csv("users.csv")
+    parse_csv::parse_csv("users.csv");
 }
--- src/parse_csv.rs
+++ src/parse_csv.rs
@@ -1,19 +1,24 @@
-use std::io;
+use serde_json::{Value,Map};
+use crate::convert_to_json::convert_to_json;
 use crate::get_csv_data::get_csv_data;
 use crate::get_headers;
+use crate::save_file_to_json::save_file_to_json;
 
 pub fn parse_csv(filename: &str) {
-    let contents=std::fs::read_to_string((filename)).expect("Something went wrong reading the file");
+    let contents=std::fs::read_to_string(filename).expect("Something went wrong reading the file");
     let headers;
     let first_line=contents.lines().next().unwrap();
     headers=get_headers::get_headers(first_line.to_string());
-    println!("{:?}",headers);
+    let mut csv_data:Vec<String>=Vec::new();
     for (index,line) in contents.lines().enumerate(){
         if index==0{
-           continue; 
+           continue;
         }
         let data=get_csv_data(line.to_string());
-        println!("{:?}",data);
+        let json_data=convert_to_json(headers.clone(),data);
+        save_file_to_json(json_data.clone(),"data.json".to_string());
+        csv_data.push(json_data);
     }
+    println!("{:?}",csv_data);
     
 }
